0880e2cf5c67a90a5af685eade3db7a0ca4fa077
Adrian U <adrian@bsd-net.pl>
2022-05-13 07:15:47 +0200

    Relacje

--- Entities/MyBoardsContext.cs
+++ Entities/MyBoardsContext.cs
@@ -14,6 +14,42 @@ namespace MyBoards.Entities
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Address> Addresses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<WorkItem>()  // lepiej tak jak na dole
+            //       .Property(x => x.State)
+            //       .IsRequired();
+            //modelBuilder.Entity<WorkItem>()
+            //    .Property(x => x.Area)
+            //    .HasColumnType("varchar(200)");
+
+
+            modelBuilder.Entity<WorkItem>(eb =>
+              {
+                  eb.Property(x => x.State).IsRequired();
+                  eb.Property(x => x.Area).HasColumnType("varchar(200)");
+                  eb.Property(wi => wi.InterationPath).HasColumnName("Iteration_Path");
+                  eb.Property(wi => wi.Effort).HasColumnType("decimal(5,2)");
+                  eb.Property(wi => wi.EnddDate).HasPrecision(3);
+                  eb.Property(wi => wi.Activity).HasMaxLength(200);
+                  eb.Property(wi => wi.RemaningWork).HasPrecision(14, 2);
+                  eb.Property(wi => wi.Prioryti).HasDefaultValue(1);
+              });
+
+            modelBuilder.Entity<Comment>(eb =>
+            {
+                eb.Property(x => x.CreatedDate).HasDefaultValueSql("getutcdate()");
+                eb.Property(x => x.UpdateDate).ValueGeneratedOnUpdate();
+            });
+
+            modelBuilder.Entity<User>() // 1 do 1 relacja
+                .HasOne(u => u.Address)
+                .WithOne(u => u.User)
+                .HasForeignKey<Address>(a => a.UserId);
+
+
+        }
+
 
     }
 }
--- Entities/WorkItem.cs
+++ Entities/WorkItem.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyBoards.Entities
 {
@@ -6,16 +8,21 @@ namespace MyBoards.Entities
     {
      
         public int Id { get; set; } 
+        //[Required] // nie null
         public string State { get; set; }
+        //[Column(TypeName ="varchar(200)")] // zmiana typu
         public string Area { get; set; }
+        //[Column("Iteration_patch")] // zmiana nazwy kolumny
         public string InterationPath { get; set; }
         public int Prioryti { get; set; }
         public DateTime? StardDate { get; set; }
+        //[Precision(3)] // precyzja
         public DateTime? EnddDate { get; set; }
+        //[Column(TypeName = "decimal(5,2)")] // zmiana typu
         public decimal Effort { get; set; }
-
+        //[MaxLength(200)]  // maksymalna długosc
         public string Activity { get; set; }
-
+        //[Precision(14,2)] // precyzja
         public decimal RemaningWork { get; set; }
         public string Type { get; set; }
 
