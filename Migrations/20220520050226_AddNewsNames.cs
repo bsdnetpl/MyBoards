144ec36488948b925908e59d040d35aea553b59f
Adrian U <adrian@bsd-net.pl>
2022-05-20 07:28:23 +0200

    rename

---
+++ Migrations/20220520050226_AddNewsNames.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class AddNewsNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+               name: "EnddDate",
+               table: "WorkItem",
+               newName: "EndDate");
+
+            migrationBuilder.RenameColumn(
+               name: "Effort",
+               table: "WorkItem",
+               newName: "Efford");
+
+            migrationBuilder.RenameColumn(
+               name: "Prioryti",
+               table: "WorkItem",
+               newName: "Priority");
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+               name: "EndDate",
+               table: "WorkItem",
+               newName: "EnddDate");
+
+            migrationBuilder.RenameColumn(
+               name: "Efford",
+               table: "WorkItem",
+               newName: "Effort");
+
+            migrationBuilder.RenameColumn(
+               name: "Priority",
+               table: "WorkItem",
+               newName: "Prioryti");
+        }
+    }
+}
